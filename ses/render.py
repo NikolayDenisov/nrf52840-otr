89794f9fd462ef015144eb5e3ad54f2649410768
Nikolay Denisov <nickdenisov@protonmail.com>
2021-11-06 00:22:00 +0500

    Add(render): new variable

--- ses/render.py
+++ ses/render.py
@@ -26,6 +26,7 @@ for dir_name in include_str.split():
     includes.append(get_rpath(dir_name, sdk_path).replace('\\', '/'))
 includes.append('config')
 c_user_include_directories = ';'.join(includes)
+c_preprocessor_definitions = config.get("c_preprocessor_definitions", "list").replace('\n', ';')[1:]
 with open('template.project') as f:
     tmp = f.read()
 template = Template(tmp)
@@ -40,13 +41,15 @@ for i in range(1, 10):
         folder_content = list()
         for item in folder_list.split():
             try:
-                folder_content.append(get_rpath(item, sdk_path).replace('\\', '/'))
+                folder_content.append(
+                    get_rpath(item, sdk_path).replace('\\', '/'))
             except AttributeError as e:
                 pass
         folders[folder_name] = folder_content
 folders['Application'] = ['../main.c', 'config/sdk_config.h']
 output = template.render(name=project_name, c_user_include_directories=c_user_include_directories, folders=folders,
-                      sdk_path=get_rpath('', sdk_path).replace('\\', '/'))
+                         sdk_path=get_rpath('', sdk_path).replace('\\', '/'),
+                         c_preprocessor_definitions=c_preprocessor_definitions)
 
 with open("{}.emProject".format(project_name), "w") as fh:
     fh.write(output)

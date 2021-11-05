522e3ecd5990be37b5bef1d4702a29375cdc5625
Nikolay Denisov <nickdenisov@protonmail.com>
2021-11-06 00:58:39 +0500

    Fixed(render): First working render is a temp example

--- main.c
+++ main.c
@@ -1,134 +1,51 @@
-/**
- * Copyright (c) 2015 - 2021, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- * @defgroup blinky_example_main main.c
- * @{
- * @ingroup blinky_example_freertos
- *
- * @brief Blinky FreeRTOS Example Application main file.
- *
- * This file contains the source code for a sample application using FreeRTOS to blink LEDs.
- *
- */
-
 #include <stdbool.h>
 #include <stdint.h>
-
-#include "FreeRTOS.h"
-#include "task.h"
-#include "timers.h"
-#include "bsp.h"
-#include "nordic_common.h"
-#include "nrf_drv_clock.h"
-#include "sdk_errors.h"
+#include <stdio.h>
+#include "nrf.h"
+#include "nrf_delay.h"
+#include "nrf_temp.h"
 #include "app_error.h"
+#include "bsp.h"
 
-#if LEDS_NUMBER <= 2
-#error "Board is not equipped with enough amount of LEDs"
-#endif
-
-#define TASK_DELAY        200           /**< Task delay. Delays a LED0 task for 200 ms */
-#define TIMER_PERIOD      1000          /**< Timer period. LED1 timer will expire after 1000 ms */
-
-TaskHandle_t  led_toggle_task_handle;   /**< Reference to LED0 toggling FreeRTOS task. */
-TimerHandle_t led_toggle_timer_handle;  /**< Reference to LED1 toggling FreeRTOS timer. */
-
-/**@brief LED0 task entry function.
- *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the task.
- */
-static void led_toggle_task_function (void * pvParameter)
-{
-    UNUSED_PARAMETER(pvParameter);
-    while (true)
-    {
-        bsp_board_led_invert(BSP_BOARD_LED_0);
-
-        /* Delay a task for a given number of ticks */
-        vTaskDelay(TASK_DELAY);
-
-        /* Tasks must be implemented to never return... */
-    }
-}
-
-/**@brief The function to call when the LED1 FreeRTOS timer expires.
- *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the timer.
- */
-static void led_toggle_timer_callback (void * pvParameter)
-{
-    UNUSED_PARAMETER(pvParameter);
-    bsp_board_led_invert(BSP_BOARD_LED_1);
-}
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
 
 int main(void)
 {
-    ret_code_t err_code;
+    // This function contains workaround for PAN_028 rev2.0A anomalies 28, 29,30 and 31.
+    int32_t volatile temp;
 
-    /* Initialize clock driver for better time accuracy in FREERTOS */
-    err_code = nrf_drv_clock_init();
-    APP_ERROR_CHECK(err_code);
+    nrf_temp_init();
 
-    /* Configure LED-pins as outputs */
-    bsp_board_init(BSP_INIT_LEDS);
-
-    /* Create task for LED0 blinking with priority set to 2 */
-    UNUSED_VARIABLE(xTaskCreate(led_toggle_task_function, "LED0", configMINIMAL_STACK_SIZE + 200, NULL, 2, &led_toggle_task_handle));
-
-    /* Start timer for LED1 blinking */
-    led_toggle_timer_handle = xTimerCreate( "LED1", TIMER_PERIOD, pdTRUE, NULL, led_toggle_timer_callback);
-    UNUSED_VARIABLE(xTimerStart(led_toggle_timer_handle, 0));
-
-    /* Activate deep sleep mode */
-    SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
-
-    /* Start FreeRTOS scheduler. */
-    vTaskStartScheduler();
+    APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+    NRF_LOG_INFO("Temperature example started.");
 
     while (true)
     {
-        /* FreeRTOS should not be here... FreeRTOS goes back to the start of stack
-         * in vTaskStartScheduler function. */
+        NRF_TEMP->TASKS_START = 1; /** Start the temperature measurement. */
+
+        /* Busy wait while temperature measurement is not finished, you can skip waiting if you enable interrupt for DATARDY event and read the result in the interrupt. */
+        /*lint -e{845} // A zero has been given as right argument to operator '|'" */
+        while (NRF_TEMP->EVENTS_DATARDY == 0)
+        {
+            // Do nothing.
+        }
+        NRF_TEMP->EVENTS_DATARDY = 0;
+
+        /**@note Workaround for PAN_028 rev2.0A anomaly 29 - TEMP: Stop task clears the TEMP register. */
+        temp = (nrf_temp_read() / 4);
+
+        /**@note Workaround for PAN_028 rev2.0A anomaly 30 - TEMP: Temp module analog front end does not power down when DATARDY event occurs. */
+        NRF_TEMP->TASKS_STOP = 1; /** Stop the temperature measurement. */
+
+        NRF_LOG_INFO("Actual temperature: %d", (int)temp);
+        nrf_delay_ms(500);
+
+        NRF_LOG_FLUSH();
     }
 }
 
-/**
- *@}
- **/
+
+/** @} */
--- ses/render.py
+++ ses/render.py
@@ -24,7 +24,6 @@ includes.append('../config')
 include_str = config.get("c_user_include_directories", "list")
 for dir_name in include_str.split():
     includes.append(get_rpath(dir_name, sdk_path).replace('\\', '/'))
-includes.append('config')
 c_user_include_directories = ';'.join(includes)
 c_preprocessor_definitions = config.get("c_preprocessor_definitions", "list").replace('\n', ';')[1:]
 with open('template.project') as f:
@@ -46,7 +45,7 @@ for i in range(1, 10):
             except AttributeError as e:
                 pass
         folders[folder_name] = folder_content
-folders['Application'] = ['../main.c', 'config/sdk_config.h']
+folders['Application'] = ['../main.c', '../config/sdk_config.h']
 output = template.render(name=project_name, c_user_include_directories=c_user_include_directories, folders=folders,
                          sdk_path=get_rpath('', sdk_path).replace('\\', '/'),
                          c_preprocessor_definitions=c_preprocessor_definitions)
